2de0aefb756365cd03b311a7e3bc1cf182764eab
Lei Ju <leiju@google.com>
2024-09-11 14:00:57 -0700

    [tinysys] Reorg make files for tinysys
    This change
    1) moves tinysys specific flags and macros from platform/platform.mk
       to mk files of each platforms;
    2) only enforces definition of RISCV_TINYSYS_PREFIX for CHRE build;
    3) define flags to inline ctype.h functions for nanoapps, which fixes
       errors of those undefined symbols.
    
    Bug: 365864688
    Test: 1) build chre for both 55e03 and 55e300 targets successfully
          2) verified that nanoapps built have ctype.h functions inlined
    Change-Id: I7bbad8e7ca4befddb4239df78b48fc595a39fb9b

--- variant/tinysys/variant.mk
+++ variant/tinysys/variant.mk
@@ -1,3 +1,8 @@
+#
+# Google Reference CHRE framework build customization for tinysys platforms.
+#
+# Build customization of nanoapps can be found in aosp_riscv*_tinysys.mk.
+#
 
 ifeq ($(ANDROID_BUILD_TOP),)
 $(error "You should supply an ANDROID_BUILD_TOP environment variable \
